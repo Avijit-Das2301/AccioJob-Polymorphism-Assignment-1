7e774b0d336ff05b137d842d0a47e055de16a3d3
Avijit Das <avijitdas2301@gmail.com>
2025-09-13 00:36:01 +0530

    AccioJob-Polymorphism-Assignment-1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+class Main {
+    public static class Product {
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p=new Product();
+        System.out.println(p.product(5,10));
+        System.out.println(p.product(5,10,15));
+        System.out.println(p.product(2.5,5.5));
+    }
+}
